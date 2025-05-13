d8d8c02a44c4137c308cf6e0ffb4ec7804884fe6
rthh21 <andreythstan25@gmail.com>
2025-05-14 01:34:46 +0300

    Styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   def index
     # your code here
   end
+  def after_sign_in_path_for(resource)
+    # Change this to your desired path
+  end
 end
